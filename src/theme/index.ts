2e6463d6547109e7212a1932b68230fab890fcb7
jeferson-bsi <jefersoncou18@gmail.com>
2022-06-13 08:17:43 -0300

    create and configure project

---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  red: '#e52e4d',
+  blue: '#5429cc',
+  green: '#33cc95',
+  blueLight: '#6933ff',
+  textTitle: '#363f5f',
+  textBody: '#969cb3',
+  background: '#F0F2F5',
+  shape: '#FFFFFF',
+}
